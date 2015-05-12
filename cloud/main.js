a627440a864fd2ff1df0bd279f515c4f6f6ca1aa
Corwin <corwin@Corwins-MBP.local>
2015-05-12 21:19:55 +0800

    test

--- cloud/main.js
+++ cloud/main.js
@@ -3,3 +3,26 @@
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+AV.Cloud.define("GetInfoList", function(request, response){
+	var StudentId = request.params.StudentId;
+	var RequestDate = request.params.RequestDate;
+	var Depart = ParseDepart(StudentId);
+	var query = new AV.query(DeanInfo);
+	query.equalTo("Date", RequestDate);
+	query.find({
+		success: function(results){
+			response.success(results);
+		},
+		error: function(error){
+			alert("Error");
+		}
+	})
+});
+
+function ParseDepart(StudentId){
+	var Id = parseInt(StudentId);
+	Id = Id / 1000;
+	Id = Id % 100000;
+	return Id;
+}
