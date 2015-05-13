0427eeb9c5d00e2805ac3bf7caa89c4aef6edce5
Corwin <corwin@Corwins-MBP.local>
2015-05-13 10:14:39 +0800

    test

--- cloud/main.js
+++ cloud/main.js
@@ -8,24 +8,45 @@ AV.Cloud.define("GetInfoList", function(request, response){
 	var StudentId = request.params.StudentId;
 	var RequestDate = request.params.RequestDate;
 	var Depart = ParseDepart(StudentId);
+	var Result;
+
+
 	var DeanInfo = AV.Object.extend("DeanInfo");
 	var query = new AV.Query(DeanInfo);
 	query.equalTo("Date", RequestDate);
 	query.find({
 		success: function(results){
-			response.success(results);
+			Result = results;
 		},
 		error: function(error){
 			alert("Error");
 		}
 	})
-});
 
-AV.Cloud.define("GetSccList", function(request, response){
-	var RequestDate = request.params.RequestDate;
 	var SccInfo = AV.Object.extend("SccInfo");
 	var query = new AV.Query(SccInfo);
 	query.equalTo("Date", RequestDate);
+	query.find({
+		success: function(results){
+			Result += results;
+			response.success(Result);
+		},
+		error: function(error){
+			alert("Error");
+		}
+	})
+
+
+});
+
+
+AV.Cloud.define("GetCourseList", function(request, response){
+	var StudentId = request.params.StudentId;
+	var RequestDate = request.params.RequestDate;
+	var CourseInfo = AV.Object.extend("CourseInfo");
+	var query = new AV.Query(CourseInfo);
+	query.equalTo("Date", RequestDate);
+	query.equalTo("StudentId")
 	query.find({
 		success: function(results){
 			response.success(results);
@@ -35,7 +56,6 @@ AV.Cloud.define("GetSccList", function(request, response){
 		}
 	})
 });
-
 
 
 function ParseDepart(StudentId){
